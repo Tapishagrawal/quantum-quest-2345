dad05a272e576afa43659bd76635708cb4e73c78
Tapishagrawal <tapishg4@gmail.com>
2023-12-13 23:51:55 +0530

    add basic structure header sidebar navbar main section

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { BiSliderAlt } from "react-icons/bi";
+import { IoMdHeart } from "react-icons/io";
+import { IoBag } from "react-icons/io5";
 
-export const Navbar = () => {
+export const Navbar = ({handleToggleMenu}) => {
     return (
-        <div>Navbar</div>
+        <div className='flex justify-between items-center z-50'>
+            <Link className='text-xl' onClick={handleToggleMenu}>
+                <BiSliderAlt />
+            </Link>
+            <div className='flex items-center gap-4'>
+                <Link className='relative bg-[#1b2635] shadow-[0px_2px_4px_rgba(0,0,0,0.4),0px_7px_13px_-3px_rgba(0,0,0,0.3),0px_-3px_0px_inset_rgba(0,0,0,0.2)] px-4 py-[0.4rem] rounded-md '>
+                    <IoMdHeart className='text-2xl' />
+                    <span className='absolute text-[0.6rem] bottom-2 right-4 px-1 rounded-full bg-indigo-700'>0</span>
+                </Link>
+                <Link className='relative bg-[#1b2635] shadow-[0px_2px_4px_rgba(0,0,0,0.4),0px_7px_13px_-3px_rgba(0,0,0,0.3),0px_-3px_0px_inset_rgba(0,0,0,0.2)] px-4 py-[0.4rem] rounded-md '>
+                    <IoBag className='text-2xl'/>
+                    <span className='absolute text-[0.6rem] bottom-1 right-4 px-1 rounded-full bg-indigo-700'>0</span>
+                </Link>
+                <div className='flex items-center bg-[#1b2635] shadow-[0px_2px_4px_rgba(0,0,0,0.4),0px_7px_13px_-3px_rgba(0,0,0,0.3),0px_-3px_0px_inset_rgba(0,0,0,0.2)] gap-3 px-3 py-1 rounded-md'>
+                    <div>
+                        <img className='rounded-full' src="https://placehold.co/45" alt="" />
+                    </div>
+                    <div>
+                        <p>User Name</p>
+                        <p>View profile</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
