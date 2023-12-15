714c14ef391a9f62591f742bbb55c64bdb0535eb
Nehaa1210 <121329987+Nehaa1210@users.noreply.github.com>
2023-12-15 10:44:40 +0530

    Login connected

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,27 @@
 const express = require("express");
 const { connection } = require("./db");
+const { userRouter } = require("./routess/user.routess");
+
 require("dotenv").config();
 const cors = require("cors");
 
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-app.get("/", (req,res)=>{
-    res.send("hi")
-})
+app.use("/users", userRouter);
 
-app.listen(process.env.port, async()=>{
-    try {
-        await connection
-        console.log("database connected");
-        console.log(`http://localhost:${process.env.port}`)
-    } catch (error) {
-        console.log("Error in connection the Database.")
-        console.log(error)
-    }
-})
+app.get("/", (req, res) => {
+  res.send("hi");
+});
+
+app.listen(process.env.port, async () => {
+  try {
+    await connection;
+    console.log("database connected");
+    console.log(`http://localhost:${process.env.port}`);
+  } catch (error) {
+    console.log("Error in connection the Database.");
+    console.log(error);
+  }
+});
