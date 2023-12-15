f93e3207ca857a62ab98f280a3e0786e7ee5293e
Nehaa1210 <121329987+Nehaa1210@users.noreply.github.com>
2023-12-16 01:35:52 +0530

    Admin part

--- backend/routes/game.routes.js
+++ backend/routes/game.routes.js
@@ -29,6 +29,33 @@ gameRouter.get("/", async(req,res) => {
 
 
 
+gameRouter.patch("/update/:gameID", async(req,res) => {
+    const {gameID} = req.params
+    try{
+        
+            await GameModel.findByIdAndUpdate({_id:gameID},req.body) 
+            res.status(200).send({"msg":`Game with ID:${gameID} has been updated`})
+       
+    }
+    catch(err){
+        res.status(400).send({"error":err})
+    }
+})
+
+
+gameRouter.delete("/delete/:gameID", async(req,res) => {
+    const {gameID} = req.params
+    try{
+        
+            await GameModel.findByIdAndDelete({_id:gameID}) 
+            res.status(200).send({"msg":`Game with ID:${gameID} has been deleted`})
+       
+    }
+    catch(err){
+        res.status(400).send({"error":err})
+    }
+})
+
 
 module.exports={
     gameRouter
