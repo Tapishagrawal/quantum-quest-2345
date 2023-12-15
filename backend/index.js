4715705a7b13fe279aeaaaff2f91664b29681e28
Nehaa1210 <121329987+Nehaa1210@users.noreply.github.com>
2023-12-15 10:02:08 +0530

    Backend setup is done

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const { connection } = require("./db");
+const { userRouter } = require("./routess/user.routess")
 require("dotenv").config();
 const cors = require("cors");
 
 const app = express();
 app.use(cors())
 app.use(express.json());
+
+app.use("/users", userRouter)
 
 app.get("/", (req,res)=>{
     res.send("hi")
